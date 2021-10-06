aa9db6f55dc9ffe6ea681c3728f1dfdfea789172
César Fernández <csarsamo@gmail.com>
2021-10-05 21:52:31 -0500

    Conección a la BD a la consola

--- ElGordo.App/ElGordo.App.Consola/Program.cs
+++ ElGordo.App/ElGordo.App.Consola/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Text.Json;
+using ElGordo.App.Dominio;
+using ElGordo.App.Persistencia;
 
 namespace ElGordo.App.Consola
 {
     class Program
     {
+        private static IRepositorioProductos _repoProducto = new RepositorioProductos(new App.Persistencia.AppContext());
+        private static IRepositorioEstadoProducto _repoEstadoProducto = new RepositorioEstadoProducto(new App.Persistencia.AppContext());
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //AgregaProducto();
+
+            _repoProducto.CambiaEstado(1, 1);
+
+            foreach (var item in _repoEstadoProducto.GetAllEstadosProducto())
+            {
+                Console.WriteLine(item.Nombre);
+
+            }
+            //Console.WriteLine(_repoEstadoProducto.GetEstadoProducto(1).Nombre);
+            _repoEstadoProducto.AddEstadoProducto(new EstadoProducto{Nombre= "Descontinuado"});
+        }
+
+        private static void AgregaProducto()
+        {
+            var producto = new Producto
+            {
+                //Id=1,
+                Nombre = "Hamburguesa",
+                Estado = _repoEstadoProducto.GetEstadoProducto(1),
+                Descripcion = "Hamburguesa everywhere",
+                Imagen = "hamburguesa.jpg",
+                Precio = 15000,
+            };
+            Console.WriteLine(JsonSerializer.Serialize(producto));
+            //_repoProducto.AddProducto(producto);
+            //_repoProducto.UpdateProducto(producto); 
         }
     }
 }
---
+++ ElGordo.App/ElGordo.App.Persistencia/Migrations/20211006015041_Initial.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElGordo.App.Persistencia.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EstadoFactura",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstadoFactura", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstadoPedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstadoPedido", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstadoProducto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstadoProducto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nickname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Factura",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EstadoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factura", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Factura_EstadoFactura_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "EstadoFactura",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EstadoId = table.Column<int>(type: "int", nullable: true),
+                    Precio = table.Column<float>(type: "real", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Producto_EstadoProducto_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "EstadoProducto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cliente = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FacturaId = table.Column<int>(type: "int", nullable: true),
+                    EstadoId = table.Column<int>(type: "int", nullable: true),
+                    LatitudEntrega = table.Column<float>(type: "real", nullable: false),
+                    LongitudEntrega = table.Column<float>(type: "real", nullable: false),
+                    LatitudActual = table.Column<float>(type: "real", nullable: false),
+                    LongitudActual = table.Column<float>(type: "real", nullable: false),
+                    Notas = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fecha_pedido = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Fecha_preparacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Fecha_envio = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Fecha_entrega = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedido_EstadoPedido_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "EstadoPedido",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pedido_Factura_FacturaId",
+                        column: x => x.FacturaId,
+                        principalTable: "Factura",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Detalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductoId = table.Column<int>(type: "int", nullable: true),
+                    Precio = table.Column<float>(type: "real", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Impuesto = table.Column<float>(type: "real", nullable: false),
+                    FacturaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Detalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Detalle_Factura_FacturaId",
+                        column: x => x.FacturaId,
+                        principalTable: "Factura",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Detalle_Producto_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Producto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Detalle_FacturaId",
+                table: "Detalle",
+                column: "FacturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Detalle_ProductoId",
+                table: "Detalle",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Factura_EstadoId",
+                table: "Factura",
+                column: "EstadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_EstadoId",
+                table: "Pedido",
+                column: "EstadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_FacturaId",
+                table: "Pedido",
+                column: "FacturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producto_EstadoId",
+                table: "Producto",
+                column: "EstadoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Detalle");
+
+            migrationBuilder.DropTable(
+                name: "Pedido");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Producto");
+
+            migrationBuilder.DropTable(
+                name: "EstadoPedido");
+
+            migrationBuilder.DropTable(
+                name: "Factura");
+
+            migrationBuilder.DropTable(
+                name: "EstadoProducto");
+
+            migrationBuilder.DropTable(
+                name: "EstadoFactura");
+        }
+    }
+}
