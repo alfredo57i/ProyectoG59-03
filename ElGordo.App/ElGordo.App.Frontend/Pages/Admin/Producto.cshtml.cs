b7d408353d13732a9c43ceff3ba522ddbf70e76a
alfredo57i <alfredo57a@gmail.com>
2021-10-13 12:08:09 -0500

    Versión 1.0

--- ElGordo.App/ElGordo.App.Frontend/Pages/Admin/Producto.cshtml.cs
+++ ElGordo.App/ElGordo.App.Frontend/Pages/Admin/Producto.cshtml.cs
@@ -10,56 +10,74 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ElGrodo.Frontend.Pages
 {
     [Authorize]
     public class ProductoModel : PageModel
     {
+        private readonly IWebHostEnvironment _enviroment;
         private static readonly IRepositorioProductos _repProducto = new RepositorioProductos(new ElGordo.App.Persistencia.AppContext());
         private static readonly IRepositorioEstadoProducto _repEstadoProducto = new RepositorioEstadoProducto(new ElGordo.App.Persistencia.AppContext());
         [BindProperty]
         public Producto Producto { get; set; }
+        [BindProperty]
+        public IFormFile Imagen { get; set; }
         public IEnumerable<EstadoProducto> estadosProducto = _repEstadoProducto.GetAllEstadosProducto();
-        public ProductoModel()
+        public ProductoModel(IWebHostEnvironment environment)
         {
+            _enviroment = environment;
         }
         public IActionResult OnGet(int? productoId)
         {
             //Si el productoId que llega por GET tiene un valor
-            if (productoId.HasValue){
+            if (productoId.HasValue)
+            {
                 //Busca el producto en la base de datos
                 Producto = _repProducto.GetProducto(productoId.Value);
-            }else{
+            }
+            else
+            {
                 //Crea un nuevo producto y agrega valores por defecto
-                Producto = new Producto{Estado = 1,Imagen = "default.png"};
+                Producto = new Producto { Estado = 1, Imagen = "default.png" };
             }
 
             //Si envian un Id de producto que no está en la base de datos
-            if (Producto == null){
+            if (Producto == null)
+            {
                 //Devuelve la pagina de error
                 return Redirect("/Errors/404");
-            }else{
+            }
+            else
+            {
                 return Page();
             }
         }
 
-        public IActionResult OnPost(int estado)//Estado es el nombre del Select estados.
+        public async Task<IActionResult> OnPost(int estado)//Estado es el nombre del Select estados.
         {
-            Producto.Estado=estado;
+            Producto.Estado = estado;
             //Console.WriteLine(JsonSerializer.Serialize(Producto));
             if (!ModelState.IsValid) { return Page(); }
+            
+            if (Imagen != null)
+            {//Si se ha seleccionado una imagen la sube a la carpeta "wwwroot\img"
+            // Producto.Imagen = Imagen.FileName;
+                var imgUpload = System.IO.Path.Combine(_enviroment.ContentRootPath, "wwwroot\\img", Imagen.FileName);
+                await Imagen.CopyToAsync(new System.IO.FileStream(imgUpload, System.IO.FileMode.Create));
+            }
             if (Producto.Id > 0)
             {
                 //Si el Id del producto es mayor que cero actualizar el Producto enviado
                 Producto = _repProducto.UpdateProducto(Producto);
-                ViewData["Respuesta"] = Alerts.ShowAlert(Alert.Success, "<span>"+Producto.Nombre+" fue modificado correctamente</span>");
+                ViewData["Respuesta"] = Alerts.ShowAlert(Alert.Success, "<span>" + Producto.Nombre + " fue modificado correctamente</span>");
             }
             else
             {
                 //Si el Id del Prodcuto es CERO crea un nuevo producto
                 _repProducto.AddProducto(Producto);
-                ViewData["Respuesta"] = Alerts.ShowAlert(Alert.Primary, "<span><strong>"+Producto.Nombre+"</strong> se agregó a la lista de productos</span>");
+                ViewData["Respuesta"] = Alerts.ShowAlert(Alert.Primary, "<span><strong>" + Producto.Nombre + "</strong> se agregó a la lista de productos</span>");
             }
 
             return Page();
